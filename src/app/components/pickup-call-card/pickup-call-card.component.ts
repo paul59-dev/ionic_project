6c556c19b61ce805bd6e5750d3a2d851d4808975
Paul-developpeur <paulallebee@gmail.com>
2024-01-29 18:11:31 +0100

    feat: creating reusable custom components

--- src/app/components/pickup-call-card/pickup-call-card.component.ts
+++ src/app/components/pickup-call-card/pickup-call-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,9 +7,18 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: './pickup-call-card.component.html',
   styleUrls: ['./pickup-call-card.component.scss'],
   standalone: true,
-  imports: [IonicModule]
+  imports: [IonicModule, CommonModule]
 })
 export class PickupCallCardComponent  implements OnInit {
+
+  @Input() hasHeader: boolean = false;
+  @Input() hasFooter: boolean = false;
+
+  @Input() status: string = "";
+  @Input() updatedAt: string = "";
+  @Input() createdAt: string = "";
+  @Input() notes: string = "";
+  @Input() value: string = "";
 
   constructor() { }
 
--- src/app/pages/pickup-calls/pickup-calls.page.ts
+++ src/app/pages/pickup-calls/pickup-calls.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { PickupCallCardComponent } from 'src/app/components/pickup-call-card/pickup-call-card.component';
 
 @Component({
   selector: 'app-pickup-calls',
   templateUrl: './pickup-calls.page.html',
   styleUrls: ['./pickup-calls.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, PickupCallCardComponent]
 })
 export class PickupCallsPage implements OnInit {
 
